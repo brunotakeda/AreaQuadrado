98bd482a330e28c41f0b10ab886d48312c0a67d6
brunotakeda <takedabruno@outlook.com>
2022-02-21 19:32:15 +0000

    gravado

--- Mensagem.java
+++ Mensagem.java
@@ -1,5 +1,8 @@
 public class Mensagem{
     public static void main(String[] args){
+        int idade= 25;
+            System.out.println("Bruno tem "+ (idade+ 12)+" anos");
+        
         System.out.println("Fatec Pompeia");
     }
 }
